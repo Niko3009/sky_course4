558a610456ba229c21e30fc0ebe5ee1dec188429
unknown <bairon94@mail.ru>
2022-11-01 20:28:19 +0300

    V2 (1-11-22)

--- script.js
+++ script.js
@@ -2,100 +2,178 @@
 
 application = {
   // ОБЩЕЕ
-  container: document.body.querySelector("#app"), // основной блок приложения
-  tabtitle: document.head.querySelector("title"), // тег названия вкладки
+  selectors: {
+    container: document.body.querySelector("#app"), // основной блок приложения
+    tabtitle: document.head.querySelector("title"), // тег названия вкладки
+  },
   launch: function () {
     // Отметка в консоли
+    console.log(`\n`, `ЗАПУСК ПРИЛОЖЕНИЯ\n`, `\n`);
     console.log(
-      `\n` +
-        `---------------------- ЗАПУСК ПРИЛОЖЕНИЯ ----------------------` +
-        `\n\n`
+      `\n`,
+      `Для получения текущей информации об игре, нажмите «z» \n`,
+      `\n`
     );
 
-    // Предварительные настройки
-    application.window.preparation();
+    // Предварительная...
+    application.pre.render(); // отрисовка
+    application.pre.loading(); // загрузка данных
+    application.pre.setting(); // настройка приложения
 
-    // Подгрузка данных
-    appWindowsLoad();
-    // appScreensLoad();
+    // Запуск
+    const startScreenName = "start screen";
+    console.log(
+      `\n`,
+      `―――――――――――――――――― ЭКРАН «${startScreenName.toUpperCase()}» ――――――――――――――――――\n`,
+      `\n`
+    );
+    application.state.screen = startScreenName;
 
-    // Запуск стартового окна
-    application.window.open("select");
+    application.windows.open("select"); // запуск стартового окна
+    // application.screens.open("game");
   },
-  game: { difficulty: 0 },
+  pre: {
+    render: function () {
+      application.screens.preRender();
+      application.windows.preRender();
+    },
+    loading: function () {
+      // Подгрузка экранов
+      application.screens.blocks.listFilling();
+      application.screens.listFilling();
 
-  // МОДАЛЬНОЕ ОКНО
-  window: {
+      // Подгрузка окон
+      application.windows.blocks.listFilling();
+      application.windows.listFilling();
+    },
+    setting: function () {
+      // Горячая клавиша для вывода информации
+      document.addEventListener("keydown", function (event) {
+        if (event.key === "z" || event.key === "я")
+          application.functions.getGameInfo();
+      });
+    },
+  },
+
+  state: {
+    global: null, //select, game, result
+    screen: null,
+    window: null,
+
+    difficulty: 0,
+    gameTime: 0,
+    cards: { involved: null, selected: null },
+  },
+
+  // ЭКРАНЫ
+  screens: {
     list: [],
+    listFilling: function () {},
 
-    // Открытие модального окна
-    open: function (windowName) {
-      // Проверка на наличие запрашиваемого окна
-      if (application.window.list[windowName] === undefined) {
+    // Открытие и закрытие экрана
+    open: function (screenName) {
+      // Проверка на наличие запрашиваемого экрана
+      if (application.screens.list[screenName] === undefined) {
         console.log(
           `❗`,
-          `Окна «${windowName}» в application.window.list нет !`
+          `Экрана «${screenName}» в application.screens.list нет`
         );
         return;
       }
 
-      const box = application.window.box;
-      const display = application.window.display;
-      const transitionTime = application.window.animation.duration; // время анимации окна
+      const box = application.screens.box;
+      const display = application.screens.display;
+      const screenAnimationTime = application.screens.animation.time; // время анимации экрана
+      const windowAnimationTime = application.windows.animation.time; // время анимации окна
 
-      // Отметка в консоли
-      console.log(
-        `\n` +
-          `-------------- МОДАЛЬНОЕ ОКНО «${windowName.toUpperCase()}» ----------------------` +
-          `\n\n`
-      );
+      // Закрытие текущего экрана и открытие нового (с учетом задержки на анимацию)
+      let delay = 0;
+      if (application.state.window !== null) {
+        application.windows.close();
+        delay += windowAnimationTime;
+      }
+      if (application.state.screen !== null) {
+        application.screens.close();
+        delay += screenAnimationTime;
+      }
 
-      application.window.list[windowName](box);
+      setTimeout(openNewScreen, delay * 1000);
 
-      application.functions.hiding.Off(display, transitionTime);
+      function openNewScreen() {
+        // Отметка в консоли
+        console.log(
+          `\n`,
+          `―――――――――――――――――― ЭКРАН «${screenName.toUpperCase()}» ――――――――――――――――――\n`,
+          `\n`
+        );
+
+        // Отметка в статусе приложения
+        application.state.global = screenName + ` (screen)`;
+        application.state.screen = screenName;
+
+        // Отрисовка (рендеринг)
+        application.screens.list[screenName](box); // заполнение контентом
+        application.functions.hiding.Off(display, screenAnimationTime); // анимация появления
+      }
     },
-
-    // Закрытие модального окна
     close: function () {
-      const box = application.window.box;
-      const display = application.window.display;
-      const transitionTime = application.window.animation.duration; // время анимации окна
+      const box = application.screens.box;
+      const display = application.screens.display;
+      const screenAnimationTime = application.screens.animation.time; // время анимации экрана
+      const currentScreen = application.state.screen;
 
-      application.functions.hiding.On(display, transitionTime);
+      if (currentScreen !== null) {
+        // Очистка
+        application.functions.hiding.On(display, screenAnimationTime); // анимация скрытия
+        setTimeout(() => (box.innerHTML = ""), screenAnimationTime * 1000); // очистка контента
 
-      setTimeout((box.innerHTML = ""), transitionTime * 1000);
+        // Отметка в консоли
+        console.log(
+          `\n`,
+          `Экран «${currentScreen}» закрыт ✅\n`,
+          `――――――――――――――――――――――――――――――――――――――――――――――\n`,
+          `\n`
+        );
+        console.log(`\n`, `\n`, `\n`);
+
+        // Отметка в статусе приложения
+        application.state.global = null;
+        application.state.screen = null;
+      }
     },
 
-    // Шаблон для окна
-    box: null, // само окно (область с контентом)
-    display: null, // задний фон (поверх него располагается окно)
-    preparation: function () {
+    // Шаблон для экрана
+    box: null, // сам экран (область с контентом)
+    display: null, // задний фон (поверх него располагается экран)
+    preRender: function () {
+      const app = application.selectors.container;
+
       const display = document.createElement("div");
-      display.classList.add("window_display");
-      display.style.display = "none";
-      display.style.opacity = "0";
-      application.container.appendChild(display);
+      display.classList.add("screen_display");
+      app.appendChild(display);
 
       const box = document.createElement("div");
-      box.classList.add("window_box");
+      box.classList.add("screen_box");
       display.appendChild(box);
 
-      application.window.display = display;
-      application.window.box = box;
+      application.screens.display = display;
+      application.screens.box = box;
     },
 
-    // Блоки для верстки окна
+    // Блоки для верстки экрана
     blocks: {
       list: [],
+      listFilling: function () {},
+
       render: function (
         blockName,
-        container = application.window.box,
+        container = application.screens.box,
         params = {}
       ) {
-        if (application.window.blocks.list[blockName] === undefined) {
+        if (application.screens.blocks.list[blockName] === undefined) {
           console.log(
             `❗`,
-            `Блока ${blockName} в application.window.blocks.list нет`
+            `Блока ${blockName} в application.screens.blocks.list нет`
           );
           return;
         }
@@ -108,7 +186,7 @@ application = {
         }
 
         // запускаем функцию верстки блока и сохраняем результат (если он был передан)
-        const result = application.window.blocks.list[blockName](
+        const result = application.screens.blocks.list[blockName](
           container,
           params
         );
@@ -117,8 +195,142 @@ application = {
       },
     },
 
-    //Анимация
-    animation: { duration: 1.0 },
+    //Анимация экрана
+    animation: {
+      time: 0.5, // время анимации
+    },
+  },
+
+  // МОДАЛЬНЫЕ ОКНА
+  windows: {
+    list: [],
+    listFilling: function () {},
+
+    // Открытие и закрытие окна
+    open: function (windowName) {
+      // Проверка на наличие запрашиваемого окна
+      if (application.windows.list[windowName] === undefined) {
+        console.log(
+          `❗`,
+          `Окна «${windowName}» в application.windows.list нет !`
+        );
+        return;
+      }
+
+      const box = application.windows.box;
+      const display = application.windows.display;
+      const windowAnimationTime = application.windows.animation.time; // время анимации окна
+
+      // Закрытие текущего окна и открытие нового (с учетом задержки на анимацию)
+      let delay = 0;
+      if (application.state.window !== null) {
+        application.windows.close();
+        delay += windowAnimationTime;
+      }
+
+      setTimeout(openNewWindow, delay * 1000);
+
+      function openNewWindow() {
+        // Отметка в консоли
+        console.log(
+          `\n`,
+          `··············· МОДАЛЬНОЕ ОКНО «${windowName.toUpperCase()}» ···············\n`,
+          `\n`
+        );
+
+        // Отрисовка (рендеринг)
+        application.windows.list[windowName](box); // заполнение окна контентом
+        application.functions.hiding.Off(display, windowAnimationTime); // анимация появления
+
+        // Отметка в статусе приложения
+        application.state.global = windowName + ` (window)`;
+        application.state.window = windowName;
+      }
+    },
+    close: function () {
+      const box = application.windows.box;
+      const display = application.windows.display;
+      const windowAnimationTime = application.windows.animation.time; // время анимации окна
+      const currentWindow = application.state.window;
+
+      if (currentWindow !== null) {
+        // Очистка
+        application.functions.hiding.On(display, windowAnimationTime); // анимация скрытия
+        setTimeout(() => (box.innerHTML = ""), windowAnimationTime * 1000); // очистка контента
+
+        // Отметка в консоли
+        console.log(
+          `\n`,
+          `Модальное окно «${currentWindow}» закрыто \n`,
+          `···········································\n`,
+          `\n`
+        );
+
+        // Отметка в статусе приложения
+        application.state.global = application.state.screen + ` (screen)`;
+        application.state.window = null;
+      }
+    },
+
+    // Шаблон для окна
+    box: null, // само окно (область с контентом)
+    display: null, // задний фон (поверх него располагается окно)
+    preRender: function () {
+      const app = application.selectors.container;
+
+      const display = document.createElement("div");
+      display.classList.add("window_display");
+      display.style.display = "none";
+      display.style.opacity = "0";
+      app.appendChild(display);
+
+      const box = document.createElement("div");
+      box.classList.add("window_box");
+      display.appendChild(box);
+
+      application.windows.display = display;
+      application.windows.box = box;
+    },
+
+    // Блоки для верстки окна
+    blocks: {
+      list: [],
+      listFilling: function () {},
+
+      render: function (
+        blockName,
+        container = application.windows.box,
+        params = {}
+      ) {
+        if (application.windows.blocks.list[blockName] === undefined) {
+          console.log(
+            `❗`,
+            `Блока ${blockName} в application.windows.blocks.list нет`
+          );
+          return;
+        }
+        if (container === null) {
+          console.log(
+            `❗`,
+            `В body нет поля (контейнера), введенного для ${blockName}`
+          );
+          return;
+        }
+
+        // запускаем функцию верстки блока и сохраняем результат (если он был передан)
+        const result = application.windows.blocks.list[blockName](
+          container,
+          params
+        );
+
+        if (result !== undefined) return result;
+      },
+    },
+
+    //Анимация окна
+    animation: {
+      time: 0.5, // время анимации
+    },
   },
 
   // ТАЙМЕРЫ
@@ -133,7 +345,7 @@ application = {
     },
   },
 
-  // ДОП.ФУНКЦИИ
+  // ДОП. ФУНКЦИИ
   functions: {
     // Скрытие и раскрытие блока
     hiding: {
@@ -144,21 +356,29 @@ application = {
 
         setTimeout(() => {
           block.style.removeProperty("transition");
-          block.style.removeProperty("display");
+
+          block.style.display = "none";
         }, duration * 1000);
       },
 
       // Раскрытие блока
       Off: function (block, duration) {
         block.style.removeProperty("display");
+
         setTimeout(() => {
           block.style.transition = `all ${duration}s`;
           block.style.removeProperty("opacity");
+
           setTimeout(() => {
             block.style.removeProperty("transition");
           }, duration * 1000);
         }, 10);
       },
+    },
+    // Получение информации о глобальном состоянии игры
+    getGameInfo: function () {
+      console.log(application.state);
+      return application.state;
     },
   },
 };
